72efb6a1081ee48b6394a845cccae91fc22b0e71
Razshal <fontenille.mathieu@gmail.com>
2019-05-29 15:24:59 +0200

    ex01 - half way

---
+++ d07/Assets/Scripts/CanonScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonScript : MonoBehaviour {
+    public float rotationSpeed = 1;
+    public GameObject rotaryParticle;
+    public GameObject missileParticle;
+    public GameObject canonParticle;
+    public AudioClip gunShoot;
+    public int fireRange = 100;
+    public float rotaryFireRate = 0.1f;
+    private Vector3 direction;
+    private RaycastHit raycastHit;
+    private AudioSource audioSource;
+
+    private void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+    private void Fire()
+    {
+        canonParticle.SetActive(true);
+        canonParticle.GetComponent<ParticleSystem>().time = 0;
+        PlaySound(gunShoot);
+        if (Physics.Raycast(transform.position, -transform.right, out raycastHit, fireRange))
+        {
+            Instantiate(rotaryParticle, raycastHit.point, transform.rotation);
+        }
+    }
+
+	private void Start()
+	{
+        audioSource = gameObject.GetComponent<AudioSource>();
+	}
+
+	private void FixedUpdate()
+	{
+        transform.Rotate(0, Input.GetAxis("Mouse X") * rotationSpeed, 0);
+	}
+
+	private void Update()
+	{
+        if (Input.GetMouseButtonDown(0))
+            InvokeRepeating("Fire", 0, rotaryFireRate);
+        else if (Input.GetMouseButtonUp(0))
+            CancelInvoke("Fire");
+	}
+}
--- d07/Assets/Scripts/PlayerScript.cs
+++ d07/Assets/Scripts/PlayerScript.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
+    private Vector3 movement;
+    public float speed = 0.1f;
+    public float rotationSpeed = 1;
+    public float boostMultiplier = 2;
+    public float boostTime;
+    public float maxBoostTime = 3;
+    public bool canBoost = true;
 
 	// Use this for initialization
 	void Start () {
-		
+        Cursor.lockState = CursorLockMode.Locked;
+        boostTime = 3;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void FixedUpdate()
+	{
+        movement = new Vector3(-Input.GetAxis("Vertical"), 0, 0) * speed * (canBoost ? boostMultiplier : 1);
+        gameObject.transform.Translate(movement);
+
+        transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+	}
+
+	private void Update()
+	{
+        // Boost handling
+        if (Input.GetKey(KeyCode.LeftShift) && boostTime > 0)
+            boostTime -= Time.deltaTime;
+        else if (boostTime < maxBoostTime)
+            boostTime += Time.deltaTime;
+        else
+            canBoost = true;
+        if (boostTime <= 0)
+            canBoost = false;
 	}
 }
